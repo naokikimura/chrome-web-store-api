935d6fc52139ff2deec0429775de050616a236a5
Naoki Kimura <n.kimura.cap@gmail.com>
2020-01-25 11:58:21 +0900

    Add a test

--- test/chrome-web-store-api.spec.ts
+++ test/chrome-web-store-api.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import ChromeWebStore from '../src/chrome-web-store';
 import InAppProduct from '../src/in-app-product';
+import License from '../src/license';
 
 describe('ChromeWebStoreAPI', () => {
 	before(async function () {
@@ -26,6 +27,16 @@ describe('ChromeWebStoreAPI', () => {
       const sku = ''; // TODO:
       const inAppProduct = await (new InAppProduct(chromeWebStore, itemId, sku)).fetch();
       expect(inAppProduct).to.have.property('state');
+    });
+  });
+
+  describe.skip('License', () => {
+    it('should return License Resource', async function() {
+			const chromeWebStore = this.chromeWebStore as ChromeWebStore;
+      const appId = ''; // TODO:
+      const userId = ''; // TODO:
+      const license = await (new License(chromeWebStore, appId, userId)).fetch();
+      expect(license).to.have.property('result');
     });
   });
 });
